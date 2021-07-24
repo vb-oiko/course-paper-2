8a08d92fd313532bb40f51800995c62339e0eb63
Vasyl Boyko <vboiko0@gmail.com>
2021-07-24 20:04:06 +0300

    refactor Seeder to class

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -5,23 +5,28 @@ import DB from "./connection";
 import PosFactory from "./factory/PosFactory";
 import SkuFactory from "./factory/SkuFactory";
 
-const newCollection = <T>(n: number, factory: () => InsertRow<T>) =>
-  Array(5).fill(null).map(factory);
+class Seeder {
+  static newCollection<T>(n: number, factory: () => InsertRow<T>) {
+    return Array(5).fill(null).map(factory);
+  }
 
-const main = async () => {
-  const db = await DB.getConnection();
+  static async main() {
+    const db = await DB.getConnection();
 
-  const posRepo = new PosRepo(db);
-  const skuRepo = new SkuRepo(db);
+    const posRepo = new PosRepo(db);
+    const skuRepo = new SkuRepo(db);
 
-  console.warn(PosFactory);
+    const stores = await posRepo.saveAll(
+      Seeder.newCollection(5, PosFactory.build)
+    );
+    const skus = await skuRepo.saveAll(
+      Seeder.newCollection(5, SkuFactory.build)
+    );
 
-  const stores = await posRepo.saveAll(newCollection(5, PosFactory.build));
-  const skus = await skuRepo.saveAll(newCollection(5, SkuFactory.build));
+    console.warn({ stores, skus });
 
-  console.warn({ stores, skus });
+    await db.end();
+  }
+}
 
-  await db.end();
-};
-
-main();
+Seeder.main();
