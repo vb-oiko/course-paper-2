a8a1d44a9015eaf7a5eebebea3532252364e080d
Vasyl Boyko <vboiko0@gmail.com>
2021-08-05 22:20:55 +0300

    add: SqlHelper class

---
+++ src/db/SqlHelper.ts
@@ -0,0 +1,85 @@
+import { format as formatDate, parse as parseDate } from "date-fns";
+import { format as formatSql } from "mysql2/promise";
+import sql, { empty, join, raw, Sql } from "sql-template-tag";
+
+export default class SqlHelper {
+  static booleanToInt(value: boolean): number {
+    return value ? 1 : 0;
+  }
+
+  static intToBoolean(value: number): boolean {
+    return value !== 0;
+  }
+
+  static dateToDateTime(date: Date = new Date()): string {
+    return formatDate(date, "yyyy-MM-dd HH-mm-ss");
+  }
+
+  static dateTimeToDate(datetime: string): Date {
+    return parseDate(datetime, "yyyy-MM-dd HH-mm-ss", new Date());
+  }
+
+  static joinWithAnd(values: Sql[]): Sql {
+    return join(
+      values.filter((value) => value.text !== ""),
+      " AND "
+    );
+  }
+
+  static getCojuctedWhereClause(conditions: Sql[]): Sql {
+    const notEmptyConditions = conditions.filter((value) => value.text !== "");
+
+    if (notEmptyConditions.length === 0) {
+      return empty;
+    }
+
+    return sql`WHERE ${join(notEmptyConditions, " AND ")}`;
+  }
+
+  static getDateRangeConditions(
+    query: DateRangeRequestData,
+    field: string
+  ): Sql[] {
+    const { from, to } = query;
+    const fromCondition = from
+      ? sql`${raw(field)} > ${this.dateToDateTime(from)}`
+      : empty;
+    const toCondition = to
+      ? sql`${raw(field)} < ${this.dateToDateTime(to)}`
+      : empty;
+
+    return [fromCondition, toCondition];
+  }
+
+  static getLimitOffsetClause(query: LimitOffsetRequestData): Sql[] {
+    const { limit, offset } = query;
+    const limitStatement = limit ? sql`LIMIT ${limit}` : empty;
+    const offsetStatement = offset ? sql`OFFSET ${offset}` : empty;
+
+    return [limitStatement, offsetStatement];
+  }
+
+  static formatSql(sql: Sql): string {
+    return formatSql(sql.sql, sql.values);
+  }
+
+  static addLimitOffsetClause(
+    sqlStatement: Sql,
+    limitOffsetQuery: LimitOffsetRequestData
+  ): Sql {
+    return join(
+      [sqlStatement, ...this.getLimitOffsetClause(limitOffsetQuery)],
+      " "
+    );
+  }
+}
+
+export interface DateRangeRequestData {
+  from?: Date;
+  to?: Date;
+}
+
+export interface LimitOffsetRequestData {
+  limit?: number;
+  offset?: number;
+}
