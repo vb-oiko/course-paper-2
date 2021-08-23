d2408e78285963481eea20a46406c7b24909cc89
Vasyl Boyko <vboiko0@gmail.com>
2021-08-23 18:57:45 +0300

    add: salary view

---
+++ src/db/view/SalaryView.ts
@@ -0,0 +1,54 @@
+import { Connection } from "mysql2/promise";
+import sql, { empty } from "sql-template-tag";
+import { PosType } from "../../types";
+import SqlHelper from "../SqlHelper";
+
+export default class SalaryView {
+  db: Connection;
+  debug: boolean;
+
+  constructor(db: Connection, debug = false) {
+    this.db = db;
+    this.debug = debug;
+  }
+
+  async getByPosIdOrPosType(
+    query: PosIdQueryRequestData | PosTypeQueryRequestData
+  ): Promise<SellerRow[]> {
+    const whereClause = SqlHelper.getCojuctedWhereClause([
+      "posId" in query ? sql`pos.id = ${query.posId}` : empty,
+      "posType" in query ? sql`pos.type = ${query.posType}` : empty,
+    ]);
+
+    const saleSqlQuery = sql`
+        SELECT
+            seller.id, seller.name, seller.salary
+        FROM
+            seller
+        JOIN
+            pos ON seller.pos_id = pos.id
+        ${whereClause}
+    `;
+
+    const [rows] = await this.db.query(saleSqlQuery);
+
+    SqlHelper.logSql(this.debug, saleSqlQuery);
+    SqlHelper.log(this.debug, rows);
+
+    return rows as SellerRow[];
+  }
+}
+
+export interface PosIdQueryRequestData {
+  posId?: number;
+}
+
+export interface PosTypeQueryRequestData {
+  posType?: PosType;
+}
+
+export interface SellerRow {
+  id: number;
+  name: string;
+  salary: number;
+}
