eaac304a07b545ba6912cebfd15ffc3fcf0bac94
Vasyl Boyko <vboiko0@gmail.com>
2021-09-22 21:57:40 +0300

    add: logging of request 1

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,50 @@
+import { Connection } from "mysql2/promise";
 import DB from "./db/connection";
 import SupplierTable from "./db/table/SupplierTable";
 
-const main = async () => {
-  const db = await DB.getConnection();
+import SellerProductivityView from "./db/view/SellerProductivityView";
+import TurnoverView from "./db/view/TurnoverView";
+import { PosType } from "./types";
 
+const request1 = async (db: Connection): Promise<void> => {
   const supplierTable = new SupplierTable(db, true);
 
+  console.log(
+    "\n1. Отримати перелік і загальне число постачальників, що поставляють вказаний вид товару, або деякий товар в обсязі, не менше заданого, за весь період співпраці, або за вказаний період.\n"
+  );
+
+  console.log(
+    "\nПерелік постачальників, що поставляють вказаний вид товару в обсязі, не менше заданого, за вказаний період.\n"
+  );
   await supplierTable.getBySkuOrCategoryId({
     categoryId: 5,
     minQty: 500,
     from: new Date("2020-07-01"),
     to: new Date("2021-12-01"),
-    limit: 2,
-    offset: 3,
   });
+
+  console.log(
+    "\nПерелік постачальників, що поставляють деякий товар в обсязі, не менше заданого, за весь період.\n"
+  );
+  await supplierTable.getBySkuOrCategoryId({
+    skuId: 21,
+    minQty: 500,
+  });
+};
+
+const request15 = async (db: Connection): Promise<void> => {
+  const turnoverView = new TurnoverView(db, true);
+
+  await turnoverView.getByQuery({
+    posId: 12,
+  });
+};
+
+const main = async () => {
+  const db = await DB.getConnection();
+
+  await request1(db);
+  // await request15(db);
 
   await db.end();
 };
