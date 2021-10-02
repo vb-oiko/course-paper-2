14333773bdb140e5a7b4986a0c2d756c5b2e4b23
Vasyl Boyko <vboiko0@gmail.com>
2021-10-02 15:04:01 +0300

    add PosList

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { PossList } from "./components/PosList";
 
 const dataProvider = jsonServerProvider("http://localhost:3001/api");
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="poss" list={ListGuesser} />
+    <Resource name="poss" list={PossList} />
   </Admin>
 );
 
---
+++ client/src/components/PosList.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { List, Datagrid, TextField, NumberField } from "react-admin";
+
+export const PossList: React.FunctionComponent = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="name" />
+      <TextField source="type" />
+      <NumberField source="area" />
+      <NumberField source="utilities" />
+      <NumberField source="rent" />
+      <NumberField source="floors" />
+      <NumberField source="departments" />
+      <NumberField source="halls" />
+      <NumberField source="workplaces" />
+    </Datagrid>
+  </List>
+);
