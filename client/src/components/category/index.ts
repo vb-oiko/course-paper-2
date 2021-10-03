09f002915bd25cbfafa8f6aad0b5f9c30cf74058
Vasyl Boyko <vboiko0@gmail.com>
2021-10-03 19:15:55 +0300

    add: category create

--- client/src/components/category/index.ts
+++ client/src/components/category/index.ts
@@ -1,10 +1,12 @@
 import { ResourceProps } from "react-admin";
+import { CategoryCreate } from "./CategoryCreate";
 import { CategoryEdit } from "./CategoryEdit";
 import { CategoryList } from "./CategoryList";
 
 const category: Partial<ResourceProps> = {
   edit: CategoryEdit,
   list: CategoryList,
+  create: CategoryCreate,
 };
 
 export default category;
