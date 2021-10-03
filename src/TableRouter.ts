0b5743dbb4fa9624c3d70981167b7be598a07d88
Vasyl Boyko <vboiko0@gmail.com>
2021-10-03 17:48:56 +0300

    refactor: add TableRouter

---
+++ src/TableRouter.ts
@@ -0,0 +1,71 @@
+import { Express, Request, Response } from "express";
+import BaseTable from "./db/table/BaseTable";
+
+export default class TableRouter<T> {
+  private readonly table: BaseTable<T>;
+  private readonly app: Express;
+
+  constructor(app: Express, table: BaseTable<T>) {
+    this.app = app;
+    this.table = table;
+
+    app.get(
+      `/api/${this.table.tableName}`,
+      this.getManyRequestHandler.bind(this)
+    );
+    app.get(
+      `/api/${this.table.tableName}/:id`,
+      this.getOneRequestHandler.bind(this)
+    );
+    app.put(
+      `/api/${this.table.tableName}/:id`,
+      this.putRequestHandler.bind(this)
+    );
+    app.post(
+      `/api/${this.table.tableName}`,
+      this.createRequestHandler.bind(this)
+    );
+    app.delete(
+      `/api/${this.table.tableName}/:id`,
+      this.deleteRequestHandler.bind(this)
+    );
+  }
+
+  defaultErrorHandler(res: Response): (error: Error) => void {
+    return (error: Error) => {
+      res.status(500).json({ message: error.message });
+    };
+  }
+
+  async getManyRequestHandler(req: Request, res: Response): Promise<void> {
+    try {
+      const { list, total } = await this.table.getMany(req.query);
+      res.setHeader("X-Total-Count", total);
+      res.json(list);
+    } catch (err) {
+      this.defaultErrorHandler(res)(err);
+    }
+  }
+
+  async getOneRequestHandler(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    res.json(await this.table.getOne(id).catch(this.defaultErrorHandler(res)));
+  }
+
+  async putRequestHandler(req: Request, res: Response): Promise<void> {
+    res.json(
+      await this.table.update(req.body).catch(this.defaultErrorHandler(res))
+    );
+  }
+
+  async createRequestHandler(req: Request, res: Response): Promise<void> {
+    res.json(
+      await this.table.create(req.body).catch(this.defaultErrorHandler(res))
+    );
+  }
+
+  async deleteRequestHandler(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    res.json(await this.table.delete(id).catch(this.defaultErrorHandler(res)));
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import DB from "./db/connection";
 import PosTable from "./db/table/PosTable";
 import SellerTable from "./db/table/SellerTable";
-import BaseTable from "./db/table/BaseTable";
+import SkuTable from "./db/table/SkuTable";
+import TableRouter from "./TableRouter";
+import CategoryTable from "./db/table/CategoryTable";
 
 const main = async () => {
   const PORT = process.env.PORT || 3001;
@@ -18,71 +20,10 @@ const main = async () => {
 
   app.use(express.json());
 
-  const posTable = new PosTable(db);
-  const sellerTable = new SellerTable(db);
-
-  const defaultErrorHandler = (res: Response) => (error: Error) => {
-    res.status(500).json({ message: error.message });
-  };
-
-  const getManyRequestHandler =
-    <T>(table: BaseTable<T>) =>
-    async (req: Request, res: Response) => {
-      try {
-        const { list, total } = await table.getMany(req.query);
-        res.setHeader("X-Total-Count", total);
-        res.json(list);
-      } catch (err) {
-        defaultErrorHandler(res)(err);
-      }
-    };
-
-  const getOneRequestHandler =
-    <T>(table: BaseTable<T>) =>
-    async (req: Request, res: Response) => {
-      const { id } = req.params;
-      const entity = await table.getOne(id).catch(defaultErrorHandler(res));
-      res.json(entity);
-    };
-
-  const putRequestHandler =
-    <T>(table: BaseTable<T>) =>
-    async (req: Request, res: Response) => {
-      res.json(await table.update(req.body).catch(defaultErrorHandler(res)));
-    };
-
-  const createRequestHandler =
-    <T>(table: BaseTable<T>) =>
-    async (req: Request, res: Response) => {
-      res.json(await table.create(req.body).catch(defaultErrorHandler(res)));
-    };
-
-  const deleteRequestHandler =
-    <T>(table: BaseTable<T>) =>
-    async (req: Request, res: Response) => {
-      const { id } = req.params;
-      res.json(await table.delete(id).catch(defaultErrorHandler(res)));
-    };
-
-  app.get("/api/pos", getManyRequestHandler(posTable));
-  app.get("/api/seller", getManyRequestHandler(sellerTable));
-
-  app.get("/api/pos/:id", getOneRequestHandler(posTable));
-  app.get("/api/seller/:id", getOneRequestHandler(sellerTable));
-
-  app.put("/api/pos/:id", putRequestHandler(posTable));
-  app.put("/api/seller/:id", putRequestHandler(sellerTable));
-
-  app.post("/api/pos", createRequestHandler(posTable));
-  app.post("/api/seller", createRequestHandler(sellerTable));
-
-  app.delete("/api/pos/:id", deleteRequestHandler(posTable));
-  app.delete("/api/seller/:id", deleteRequestHandler(sellerTable));
-
-  app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
-    res.status(500);
-    res.render("error", { error: err });
-  });
+  new TableRouter(app, new PosTable(db));
+  new TableRouter(app, new SellerTable(db));
+  new TableRouter(app, new CategoryTable(db));
+  new TableRouter(app, new SkuTable(db));
 
   app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
