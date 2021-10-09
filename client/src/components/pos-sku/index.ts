a09c5a9d71e7a84d1bb7c44bd8f86fef2a9fa11e
Vasyl Boyko <vboiko0@gmail.com>
2021-10-09 18:30:50 +0300

    remove unused code

--- client/src/components/pos-sku/index.ts
+++
@@ -1,8 +0,0 @@
-import { ResourceProps } from "react-admin";
-// import { PosSkuList } from "./PosSkuList";
-
-const posSku: Partial<ResourceProps> = {
-  // list: PosSkuList,
-};
-
-export default posSku;
